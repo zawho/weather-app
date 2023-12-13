be2463d66b967bb4e853593cbf60347ccaad1c8f
zawho <zthooper2@gmail.com>
2023-12-13 09:38:30 +0800

    Add unit switch to feels like

--- src/controls.js
+++ src/controls.js
@@ -5,27 +5,37 @@ function changeTempDisplay() {
     const currentF = document.querySelector('#current-f');
     const highAndLowC = document.querySelector('#high-low-c');
     const highAndLowF = document.querySelector('#high-low-f');
+    const feelsLikeC = document.querySelector('#feels-like-c');
+    const feelsLikeF = document.querySelector('#feels-like-f');
 
     if (fBtn.className === 'selected') {
         currentF.className = 'visible';
         highAndLowF.className = 'visible';
+        feelsLikeF.className = 'visible';
         currentF.style.display = 'flex';
         highAndLowF.style.display = 'flex';
+        feelsLikeF.style.display = 'grid';
 
         currentC.className = 'invisible';
         highAndLowC.className = 'invisible';
+        feelsLikeC.className = 'invisible';
         currentC.style.display = 'none';
         highAndLowC.style.display = 'none';
+        feelsLikeC.style.display = 'none';
     } else {
         currentC.className = 'visible';
         highAndLowC.className = 'visible';
+        feelsLikeC.className = 'visible';
         currentC.style.display = 'flex';
         highAndLowC.style.display = 'flex';
+        feelsLikeC.style.display = 'grid';
 
         currentF.className = 'invisible';
         highAndLowF.className = 'invisible';
+        feelsLikeF.className = 'invisible';
         currentF.style.display = 'none';
         highAndLowF.style.display = 'none';
+        feelsLikeF.style.display = 'none';
     }
 }
 
